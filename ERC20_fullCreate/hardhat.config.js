e2c8e001444dc23a8af5f01bc054630062a13748
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-12-01 02:30:23 +0200

    Add files via upload

---
+++ ERC20_fullCreate/hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan")
+require("solidity-coverage")
+require("dotenv").config()
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    hardhat: {
+      chainId: 1337
+    }
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_KEY}`
+  }
+};
